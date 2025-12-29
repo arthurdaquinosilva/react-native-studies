cc2053a463b1ca7b7aa320096bc3318e5f5a500a
arthurdaquinosilva <arthurdaquinosilva@gmail.com>
2025-12-29 10:01:35 -0300

    (fundamental-concepts): alert

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { Button, StyleSheet } from "react-native";
+import { Alert, Button, StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Button title="Click me" onPress={() => console.log("Pressed")} />
+        <Button
+          title="Click me"
+          onPress={() =>
+            Alert.alert("Attention", "Are you sure you want to delete it", [
+              { text: "Yes", onPress: () => console.log("Yes pressed") },
+              { text: "No", onPress: () => console.log("No pressed") },
+            ])
+          }
+        />
+
+        <Button
+          title="Click me 2"
+          onPress={() =>
+            Alert.prompt(
+              "Attention",
+              "Are you sure you want to delete it",
+              (text) => console.log("The user prompt the following: ", text),
+            )
+          }
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
