ad426e130bb1324669f58b5a9f9b3838675eaac6
arthurdaquinosilva <arthurdaquinosilva@gmail.com>
2025-12-29 09:05:40 -0300

    (fundamental-concepts): view

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Hello React Native!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -12,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
