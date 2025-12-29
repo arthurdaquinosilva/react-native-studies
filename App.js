2947a435ff3e05600916205122ae9330157c4edc
arthurdaquinosilva <arthurdaquinosilva@gmail.com>
2025-12-29 09:53:03 -0300

    (fundamental-concepts): button

--- App.js
+++ App.js
@@ -1,49 +1,11 @@
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Text>Hello React Native</Text>
-        <Image
-          source={require("./assets/icon.png")}
-          style={{ width: 80, height: 80 }}
-        />
-        <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
-          <Image
-            source={{
-              width: 100,
-              height: 100,
-              uri: "https://picsum.photos/100",
-            }}
-          />
-        </TouchableWithoutFeedback>
-        <TouchableOpacity onPress={() => console.log("Image tapped")}>
-          <Image
-            source={{
-              width: 100,
-              height: 100,
-              uri: "https://picsum.photos/100",
-            }}
-          />
-        </TouchableOpacity>
-        <TouchableHighlight onPress={() => console.log("Image tapped")}>
-          <Image
-            source={{
-              width: 100,
-              height: 100,
-              uri: "https://picsum.photos/100",
-            }}
-          />
-        </TouchableHighlight>
+        <Button title="Click me" onPress={() => console.log("Pressed")} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
