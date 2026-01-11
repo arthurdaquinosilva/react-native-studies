61fe044320acdb31e6ed49e61f821a0ad99818d9
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-01-11 01:00:56 -0300

    (layout): flexbox

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
-import { useDeviceOrientation } from "@react-native-community/hooks";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: "100%",
-            height: landscape ? "100%" : "30%",
-          }}
-        ></View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "pink",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
