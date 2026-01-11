ef2add8306074710f1da9074fc5e6a5a1a3f51a8
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-01-11 01:18:58 -0300

    (layout): flexbox - flexWrap and alignContent

--- App.js
+++ App.js
@@ -8,27 +8,41 @@ export default function App() {
         flex: 1,
         flexDirection: "row",
         justifyContent: "center",
-        alignItems: "center",
+        alignContent: "center", // only works if 'flexWrap is set to wrap
+        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
-          alignSelf: "flex-start",
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 200,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "grey",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
